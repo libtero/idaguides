eba813f5f2f40f94449e3251e84a159c4b47af5a
tero <arttson@gmail.com>
2025-12-28 00:18:17 +0100

    fixed: broken indent level with collapsed blocks fixed: tokens max split get_usr_indent() changed: version to 1.1.0

--- main.py
+++ main.py
@@ -27,18 +27,18 @@ class Line():
 def get_usr_indent() -> int:
 	if pdir := ida_diskio.get_user_idadir():
 		cfg = Path(pdir) / "cfg/hexrays.cfg"
-		
+
 		if cfg.exists():
 			try:
 				if data := open(cfg, "r").read():
 					for ln in data.splitlines():
 						if ln.strip().startswith("BLOCK_INDENT"):
-							if tokens := ln.split("=", 1):
+							if tokens := ln.split("="):
 								if len(tokens) == 2:
 									return int(tokens[1].strip(), 10)
 			except:
 				pass
-							
+
 	return 2
 
 
@@ -48,7 +48,7 @@ def count_indents(lines: list[str]) -> list[int]:
 	singleshot = 0
 	switchtrace = list()
 	colontrace = False
-	
+
 	for i in range(len(lines) - 1):
 		levels[i] = max(indent, 0) + singleshot
 		singleshot = 0
@@ -59,36 +59,37 @@ def count_indents(lines: list[str]) -> list[int]:
 		if colontrace and ln.endswith(";"):
 			indent -= 1
 			colontrace = False
-		
-		if ln.startswith(("if", "else", "do")):
-			if not nextln.startswith("{") and nextln.endswith(";"):
-				singleshot = 1
-			elif not len(nextln):
-				colontrace = True
-				indent += 1
 
-		elif ln.startswith("for") and ln.endswith(")"):
-			if not nextln.startswith("{"):
-				singleshot = 1
-				
-		elif ln.startswith("switch") and not ln.endswith(";"):
-			switchtrace.append(indent)
-			indent += 2
-					
-		elif ln.startswith("{"):
-			if not prevln.startswith("switch") and not prevln.endswith(";"):
-				indent += 1
+		if not ln.endswith("..."):
+			if ln.startswith(("if", "else", "do")):
+				if not nextln.startswith("{") and nextln.endswith(";"):
+					singleshot = 1
+				elif not len(nextln):
+					colontrace = True
+					indent += 1
+
+			elif ln.startswith("for") and ln.endswith(")"):
+				if not nextln.startswith("{"):
+					singleshot = 1
+
+			elif ln.startswith("switch") and not ln.endswith(";"):
+				switchtrace.append(indent)
+				indent += 2
+
+			elif ln.startswith("{"):
+				if not prevln.startswith("switch") and not prevln.endswith(";"):
+					indent += 1
 
 		if nextln.startswith("case") and nextln.endswith(":"):
 			singleshot = -1
-			
+
 		if nextln.startswith("}"):
 			if switchtrace and indent - 2 == switchtrace[-1]:
 				indent -= 2
 				switchtrace.pop()
 			else:
 				indent -= 1
-				
+
 	return levels
 
 
@@ -122,7 +123,7 @@ def draw_lines(cfunc: ida_hexrays.cfunc_t):
 	lines = [line for line in cfunc.get_pseudocode()]
 	lines_stripped = [ida_lines.tag_remove(line.line).strip() for line in lines]
 	indents = count_indents(lines_stripped)
-	
+
 	for i, line in enumerate(lines):
 		n = 1
 		if is_label_ln(line.line):
@@ -130,7 +131,7 @@ def draw_lines(cfunc: ida_hexrays.cfunc_t):
 			n = 0
 		elif is_empty_ln(line.line):
 			line.line += Liner.GAP * indents[i]
-		
+
 		line.line = line.line.replace(Liner.GAP, Liner.LINE, indents[i]).replace(Liner.LINE, Liner.GAP, n)
 
 
@@ -150,7 +151,7 @@ class IDAGuides_Plugin(idaapi.plugin_t):
 	def init(self):
 		if not idaapi.init_hexrays_plugin():
 			return idaapi.PLUGIN_SKIP
-		
+
 		init_liner()
 		self.hook = IDAGuides() # type: ignore
 		self.hook.hook()
